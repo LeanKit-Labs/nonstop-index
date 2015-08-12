0b7c071791b7ce69b2a321de01679219ca9fc10c
Alex Robson <asrobson@gmail.com>
2015-08-11 22:06:07 -0500

    Add command for running service directly

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-var host = require( 'autohost' );
+var autohost = require( 'autohost' );
+var host;
 var hyped = require( 'hyped' )();
 var authProvider = require( 'autohost-nedb-auth' )( {} );
 var config = require( './config.js' );
@@ -6,16 +7,14 @@ var daedalus = require( 'daedalus' )( 'nonstop-host', config.consul );
 
 function start() {
 	try {
-		host.init( { 
-			port: config.nonstop.host.port, 
+		host = hyped.createHost( autohost, {
+			port: config.nonstop.host.port,
 			socketIO: true,
 			origin: 'nonstop',
 			modules: [ 'nonstop-package-resource' ],
-			urlStrategy: hyped.urlStrategy,
-			noOptions: true
-		}, authProvider )
-		.then( hyped.addResources );
-		hyped.setupMiddleware( host );
+			authProvider: authProvider
+		} );
+		host.start();
 		daedalus.register( config.nonstop.host.port, [ '0.1.0', 'nonstop', 'hub' ] );
 	} catch( err ) {
 		console.log( 'Starting server failed with', err.stack );
